ad020c4a27d29a1018cf50fd6c99d191fc07d004
ashkanbossani <abossani@hotmail.com>
2022-10-27 14:23:41 -0700

    added routes and home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </BrowserRouter>  );
 }
 
 export default App;
