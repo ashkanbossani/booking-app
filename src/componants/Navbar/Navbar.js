7210c8a42dc31f2a468f97747d6c0e719da19575
ashkanbossani <abossani@hotmail.com>
2022-10-27 14:48:23 -0700

    made starting nav bar and styled

--- src/componants/Navbar/Navbar.js
+++ src/componants/Navbar/Navbar.js
@@ -5,7 +5,7 @@ function Navbar(props) {
     return (
         <div className="navbar">
             <div className="navbar__container">
-                <span className="navbar__logo">Booking</span>
+                <span className="navbar__logo">Booking.com</span>
                 <div className="navbar__items">
                     <button className="navbar__button">Login</button>
                     <button className="navbar__button">Sign Up</button>
