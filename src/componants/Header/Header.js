735842a45be9d3ad48485879a84655b0c2196ca0
ashkanbossani <abossani@hotmail.com>
2022-10-27 14:55:18 -0700

    added header to home page and installed font awesome package

---
+++ src/componants/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.scss";
+
+function Header(props) {
+  return <div className="header">
+        <div className="header__list">
+            <div className="header__list--item">
+            </div>
+
+        </div>
+  </div>;
+}
+
+export default Header;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import "./Home.scss"
-import Navbar from '../../componants/Navbar/Navbar';
+import React from "react";
+import "./Home.scss";
+import Navbar from "../../componants/Navbar/Navbar";
+import Header from "../../componants/Header/Header";
 function Home(props) {
-    return (
-        <div>
-        <Navbar/>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
 }
 
 export default Home;
