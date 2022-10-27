07f6e5f115cfad2e897dd0df734820d711f81eef
ashkanbossani <abossani@hotmail.com>
2022-10-27 14:27:26 -0700

    finished router set up for home page list of hotels and hotel with id

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import {
   Route,
 } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import List from "./pages/List/List";
+import Hotel from "./pages/Hotel/Hotel";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/hotels" element={<List/>} />
+        <Route path="/hotels/:id" element={<Hotel/>} />
       </Routes>
     </BrowserRouter>  );
 }
