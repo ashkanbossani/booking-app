529067e771deffc78907c5a59f5581914bfce024
ashkanbossani <abossani@hotmail.com>
2022-10-27 15:15:41 -0700

    added fonts and markup for header componant

--- src/componants/Header/Header.js
+++ src/componants/Header/Header.js
@@ -9,27 +9,32 @@ import { faTaxi } from "@fortawesome/free-solid-svg-icons";
 function Header(props) {
   return (
     <div className="header">
-      <div className="header__list">
-        <div className="header__list--item">
-          <FontAwesomeIcon icon={faBed} />
-          <span>Stays</span>
+      <div className="header__container">
+        <div className="header__list">
+          <div className="header__list--item active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="header__list--item">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="header__list--item">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car Rentals</span>
+          </div>
+          <div className="header__list--item">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="header__list--item">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport Taxis</span>
+          </div>
         </div>
-        <div className="header__list--item">
-          <FontAwesomeIcon icon={faPlane} />
-          <span>Flights</span>
-        </div>
-        <div className="header__list--item">
-          <FontAwesomeIcon icon={faCar} />
-          <span>Car Rentals</span>
-        </div>
-        <div className="header__list--item">
-          <FontAwesomeIcon icon={faBed} />
-          <span>Attractions</span>
-        </div>
-        <div className="header__list--item">
-          <FontAwesomeIcon icon={faTaxi} />
-          <span>Airport Taxis</span>
-        </div>
+        <h1 className="header__title">Book unique places to stay and things to do.</h1>
+        <p className="header__description">Get rewarded for your travels - inlock instant savings of 10% or more with a free Booking.com account</p>
+        <button className="header__button">Sign In / Register</button>
       </div>
     </div>
   );
