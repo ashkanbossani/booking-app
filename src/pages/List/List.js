09f7fa494a516d3d885883da6a5d1420db9481c2
ashkanbossani <abossani@hotmail.com>
2022-10-27 17:10:30 -0700

    added header and navbar to hotels page

--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import "./List.scss";
+import Navbar from "../../componants/Navbar/Navbar";
+import Header from "../../componants/Header/Header";
 
 function List(props) {
     return (
         <div>
-            
+            <Navbar/>
+            <Header/>
         </div>
     );
 }
