1dea041e7bb3a112a46b88d79abd11a2484cdca5
ashkanbossani <abossani@hotmail.com>
2022-11-02 13:53:03 -0700

    added property type list with images and description

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,13 +3,16 @@ import "./Home.scss";
 import Navbar from "../../componants/Navbar/Navbar";
 import Header from "../../componants/Header/Header";
 import Featured from "../../componants/Featured/Featured";
+import PropertyList from "../../componants/PropertyList/PropertyList";
 function Home(props) {
   return (
     <div>
       <Navbar />
       <Header />
-      <div className="home__container">
+      <div className="home">
         <Featured />
+        <h1 className="home__title">Browse by property type</h1>
+        <PropertyList />
       </div>
     </div>
   );
