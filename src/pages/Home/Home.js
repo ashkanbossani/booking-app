9a1febb5facfc53a76d4bc06747d9f0925a65672
ashkanbossani <abossani@hotmail.com>
2022-11-02 13:41:06 -0700

    added 3 stay lists with pictures via booking.com

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./Home.scss";
 import Navbar from "../../componants/Navbar/Navbar";
 import Header from "../../componants/Header/Header";
+import Featured from "../../componants/Featured/Featured";
 function Home(props) {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className="home__container">
+        <Featured />
+      </div>
     </div>
   );
 }
