2db2d19355435ba48874ca85a6974cd9e21751c4
ashkanbossani <abossani@hotmail.com>
2023-03-17 13:47:30 -0700

    finished styling for featured protperties section

--- src/componants/featuredProperties/FeaturedProperties.js
+++ src/componants/featuredProperties/FeaturedProperties.js
@@ -5,7 +5,9 @@ const FeaturedProperties = () => {
   return (
     <div className="featuredProperties">
       <div className="featuredProperties__item">
-        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" />
+        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+        alt="hotel image"
+        className="featuredProperties__image" />
         <span className="featuredProperties__Name">
           Aparthotel Stare Miasto
         </span>
@@ -17,7 +19,9 @@ const FeaturedProperties = () => {
         </div>
       </div>
       <div className="featuredProperties__item">
-        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" />
+        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+        alt="hotel image"
+        className="featuredProperties__image" />
         <span className="featuredProperties__Name">
           Aparthotel Stare Miasto
         </span>
@@ -29,7 +33,9 @@ const FeaturedProperties = () => {
         </div>
       </div>
       <div className="featuredProperties__item">
-        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" />
+        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" 
+        alt="hotel image"
+        className="featuredProperties__image"/>
         <span className="featuredProperties__Name">
           Aparthotel Stare Miasto
         </span>
@@ -41,7 +47,9 @@ const FeaturedProperties = () => {
         </div>
       </div>
       <div className="featuredProperties__item">
-        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" />
+        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1" 
+        alt="hotel image"
+        className="featuredProperties__image"/>
         <span className="featuredProperties__Name">
           Aparthotel Stare Miasto
         </span>
